6b3d975ae5baf135d8d24bb3513a77a3d1c5a503
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-03 20:13:50 -0300

    Creating a FundoGradiente component  from Styled Components and styling it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
+
+const FundoGradiente = styled.div`
+    background: linear-gradient(
+        174.61deg,
+        #041833 4.16%,
+        #04244f 48%,
+        #154580 96.76%
+    );
+    width: 100%;
+    min-height: 100vh;
+`;
+
 function App() {
-    return <></>;
+    return <FundoGradiente></FundoGradiente>;
 }
 
 export default App;
