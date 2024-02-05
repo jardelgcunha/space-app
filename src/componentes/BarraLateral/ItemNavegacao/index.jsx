86e0200d95164b2196bfd46da2ab2a1b622adb70
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-05 07:26:26 -0300

    Importing fonts and styling

--- src/componentes/BarraLateral/ItemNavegacao/index.jsx
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -5,13 +5,20 @@ const ItemListaEstilizado = styled.li`
     line-height: 29px;
     margin-bottom: 30px;
     cursor: pointer;
-    color: ${props => props.$ativo ? '#7B78E5' : '#D9D9D9'};
+    color: ${(props) => (props.$ativo ? '#7B78E5' : '#D9D9D9')};
+    font-family: ${(props) =>
+        props.$ativo ? 'GandhiSansBold' : 'GandhiSansRegular'};
     display: flex;
     align-items: center;
     gap: 22px;
 `;
 
-const ItemNavegacao = ({ children, iconeAtivo, iconeInativo, ativo = false }) => {
+const ItemNavegacao = ({
+    children,
+    iconeAtivo,
+    iconeInativo,
+    ativo = false,
+}) => {
     return (
         <ItemListaEstilizado $ativo={ativo}>
             <img src={ativo ? iconeAtivo : iconeInativo} />
