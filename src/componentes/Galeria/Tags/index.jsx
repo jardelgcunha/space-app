8dc7965b364c9abac1806655246d02ff31565525
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-06 10:32:04 -0300

    Styling Tags component

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,45 @@
+import styled from 'styled-components';
+import tags from './tags.json';
+
+const TagContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 25px;
+    margin-top: 50px;
+    margin-bottom: 10px;
+`;
+
+const TagTitulo = styled.h3`
+    color: #d9d9d9;
+    font-size: 24px;
+    font-weight: 400;
+    margin: 0;
+    margin-right: 30px;
+`;
+
+const BotaoEstilizado = styled.button`
+    font-size: 24px;
+    color: #ffffff;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    border: 2px solid transparent;
+    &:hover {
+        border-color: #c98cf1;
+    }
+`;
+
 const Tags = () => {
-    return <p>Busque por tags:</p>;
+    return (
+        <TagContainer>
+            <TagTitulo>Busque por tags:</TagTitulo>
+            {tags.map((tag) => (
+                <BotaoEstilizado key={tag.id}>{tag.titulo}</BotaoEstilizado>
+            ))}
+        </TagContainer>
+    );
 };
 
 export default Tags;
