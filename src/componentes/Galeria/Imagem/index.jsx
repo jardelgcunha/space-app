17d21b47ba23373b107c58e540d429408e339678
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-06 15:00:16 -0300

    Creating Imagem component

---
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -0,0 +1,17 @@
+const Imagem = ({ foto }) => {
+    return (
+        <figure>
+            <img src="" alt="" />
+            <figcaption>
+                <h3>Título</h3>
+                <footer>
+                    <p>Fonte</p>
+                    <button>Favorito</button>
+                    <button>Expandir</button>
+                </footer>
+            </figcaption>
+        </figure>
+    );
+};
+
+export default Imagem;
