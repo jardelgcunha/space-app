58a02a6a1e0f1b2ddb182ca2689e6e7a3217d0e8
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-06 15:01:56 -0300

    Starting the structure of image gallery
    - Using useState;
    - Importing images;
    - Styling.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Banner from './componentes/Banner';
 import bannerBackground from './assets/banner.png';
 import Galeria from './componentes/Galeria';
 
+import fotos from './fotos.json';
+import { useState } from 'react';
+
 const FundoGradiente = styled.div`
     background: linear-gradient(
         174.61deg,
@@ -34,7 +37,9 @@ const ConteudoGaleria = styled.section`
     flex-grow: 1;
 `;
 
-function App() {
+const App = () => {
+    const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+
     return (
         <FundoGradiente>
             <EstilosGlobais />
@@ -47,12 +52,12 @@ function App() {
                             texto="A galeria mais completa de fotos do espaço!"
                             backgroundImage={bannerBackground}
                         />
-                        <Galeria />
+                        <Galeria fotos={fotosDaGaleria} />
                     </ConteudoGaleria>
                 </MainContainer>
             </AppContainer>
         </FundoGradiente>
     );
-}
+};
 
 export default App;
