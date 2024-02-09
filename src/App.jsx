ede4a2eb2e979668b631366696959131c9f2c967
Jardel Cunha <jardelgcunha.ti@gmail.com>
2024-02-09 00:14:27 -0300

    Creating a function to close the modal and styling

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,10 @@ const App = () => {
                     </ConteudoGaleria>
                 </MainContainer>
             </AppContainer>
-            <ModalZoom foto={fotoSelecionada} />
+            <ModalZoom
+                foto={fotoSelecionada}
+                aoFechar={() => setFotoSelecionada(null)}
+            />
         </FundoGradiente>
     );
 };
